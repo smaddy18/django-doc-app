dec99f4337fecab3c9e0e9d4a6f47a31cf021c7e
fabri <fabrice.fkouame@gmail.com>
2023-03-11 21:32:18 +0100

    feat: adding the templates directory and his index.html file in the listings app

--- docListing/listings/views.py
+++ docListing/listings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
     return HttpResponse('<h1>Hello Django!</h1>')

dd796ef3839504994d267d5bbebd44676a56649b
fabri <fabrice.fkouame@gmail.com>
2023-03-12 00:49:48 +0100

    refactor: split the templates files into base.html and index.html

--- docListing/listings/views.py
+++ docListing/listings/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
 
 def index(request):
-    context = {"message": "Intégration bootstrap réussie !!!"}
-    template = loader.get_template("listings/index.html")
-    return HttpResponse(template.render(context, request))
+    context = {
+        "title" : "Load data",
+        "message": "Intégration bootstrap réussie !!!",
+    }
+    return render(request, "listings/index.html", context)

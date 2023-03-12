1372dde70b522df7135f52b1ebe7105917497952
fabri <fabrice.fkouame@gmail.com>
2023-03-12 07:07:34 +0100

    feat: completing the index view in the file views.py

--- docListing/listings/views.py
+++ docListing/listings/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import forms
 
 def index(request):
+    form = forms.uploadData()
     context = {
         "title" : "Load data",
         "message": "Intégration bootstrap réussie !!!",
+        "form": form,
     }
     return render(request, "listings/index.html", context)
